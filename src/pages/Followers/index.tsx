ca22475f1977e749fe81fc397c696d8afe8e3b82
Ayrton Souza <ayrtonsouza@icloud.com>
2020-11-11 02:02:46 -0300

    initial data structure for followers page

--- src/pages/Followers/index.tsx
+++ src/pages/Followers/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
-
-import { Container } from './styles';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import {
+  Container,
+  Profile,
+  ProfileImage,
+  ProfileName,
+  ProfileFollowers,
+  ProfileFollowing,
+  TabNavigation,
+  FollowersList,
+  FollowerCard,
+  FollowerImage,
+  FollowerName,
+  ProfileButton,
+} from './styles';
 
 const Followers: React.FC = () => {
   return (
-    <Container>
-      <h1>Followers</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <Profile>
+          <ProfileImage />
+          <ProfileName />
+          <ProfileFollowers />
+          <ProfileFollowing />
+        </Profile>
+        <Followers>
+          <TabNavigation />
+          <FollowersList>
+            <FollowerCard>
+              <FollowerImage />
+              <FollowerName />
+              <ProfileButton />
+            </FollowerCard>
+          </FollowersList>
+        </Followers>
+      </Container>
+      <Footer />
+    </>
   );
 };
 
--- src/pages/Followers/styles.ts
+++ src/pages/Followers/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  padding: 20px;
+`;
+
+export const Profile = styled.div``;
+export const ProfileImage = styled.div``;
+export const ProfileName = styled.div``;
+export const ProfileFollowers = styled.div``;
+export const ProfileFollowing = styled.div``;
+export const TabNavigation = styled.div``;
+export const FollowersList = styled.div``;
+export const FollowerCard = styled.div``;
+export const FollowerImage = styled.div``;
+export const FollowerName = styled.div``;
+export const ProfileButton = styled.div``;
